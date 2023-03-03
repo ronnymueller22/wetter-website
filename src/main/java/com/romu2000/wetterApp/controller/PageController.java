7f1e94495bf508833ba5eda02618e9a667133a0b
romu <ronny.mueller@tn.mischok.academy>
2023-03-03 10:54:36 +0100

    test commit

--- src/main/java/com/romu2000/wetterApp/controller/PageController.java
+++ src/main/java/com/romu2000/wetterApp/controller/PageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+//git test
 
 import java.io.IOException;
 import java.math.BigDecimal;
